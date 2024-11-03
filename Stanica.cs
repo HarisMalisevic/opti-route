7bd40cb937d71a2bb1057d6fab37fd3ad8af2df8
Haris Mališević <malisevic.haris@gmail.com>
2024-11-03 03:03:36 +0100

    Get metode za naziv i zonu

--- Stanica.cs
+++ Stanica.cs
@@ -6,7 +6,16 @@ namespace OptiRoute
     {
         private string naziv;
 
-        private Zona zona;
+        public Zona zona;
+        public string Naziv
+        {
+            get { return naziv; }
+        }
+
+        public Zona Zona
+        {
+            get { return zona; }
+        }
         public Stanica(string naziv, Zona zona)
         {
             this.naziv = naziv;
