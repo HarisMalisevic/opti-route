0df820efe6592af4d84bd8f87c4ca2c57d4b5d75
Haris Mališević <malisevic.haris@gmail.com>
2024-11-12 15:39:53 +0100

    Bus class implemented, waiting for changes in StaticData

---
+++ Bus.cs
@@ -0,0 +1,74 @@
+
+namespace OptiRoute
+{
+    public class Bus : ITransportationMethod, ICjenovnik
+    {
+        private List<Station> supportedStations;
+
+        private int[,] travelTimesMinutes;
+        private double[,] travelPricesKM;
+
+
+
+        public Bus(List<Station> supportedStations, int[,] travelTimesMinutes, double[,] travelPricesKM)
+        {
+            this.supportedStations = supportedStations;
+            this.travelTimesMinutes = travelTimesMinutes;
+            this.travelPricesKM = travelPricesKM;
+        }
+
+        public double getCommuteDurationMinutes(Station startingStation, Station destinationStation)
+        {
+            int startIndex = supportedStations.IndexOf(startingStation);
+            int destinationIndex = supportedStations.IndexOf(destinationStation);
+
+            if (startIndex == -1 || destinationIndex == -1)
+            {
+                throw new ArgumentException("One or both of the stations are not supported by this bus.");
+            }
+
+            return travelTimesMinutes[startIndex, destinationIndex];
+        }
+
+        public SortedSet<Station> getDestinationStations(Station startingStation)
+        {
+            SortedSet<Station> destinationStations = new SortedSet<Station>();
+            int startIndex = supportedStations.IndexOf(startingStation);
+
+            if (startIndex == -1)
+            {
+                throw new ArgumentException("The starting station is not supported by this bus.");
+            }
+
+            for (int i = 0; i < travelTimesMinutes.GetLength(1); i++)
+            {
+                if (travelTimesMinutes[startIndex, i] != -1)
+                {
+                    destinationStations.Add(supportedStations[i]);
+                }
+            }
+
+            return destinationStations;
+        }
+
+        public double getPriceKM(Station startingStation, Station destinationStation)
+        {
+            int startIndex = supportedStations.IndexOf(startingStation);
+            int destinationIndex = supportedStations.IndexOf(destinationStation);
+
+            if (startIndex == -1 || destinationIndex == -1)
+            {
+                throw new ArgumentException("One or both of the stations are not supported by this bus.");
+            }
+
+            return travelPricesKM[startIndex, destinationIndex];
+        }
+
+        public SortedSet<Station> getStartingStations()
+        {
+            SortedSet<Station> startingStations = [.. supportedStations];
+
+            return startingStations;
+        }
+    }
+}
