a0cf67923e858a33c5f4c23af9b57b49ac1ce8c5
Haris Mališević <malisevic.haris@gmail.com>
2024-11-23 11:45:48 +0100

    Reordered methods

--- OptiRoute/ITransportationMethod.cs
+++ OptiRoute/ITransportationMethod.cs
@@ -1,11 +1,11 @@
 namespace OptiRoute {
     public interface ITransportationMethod {
         public double getCommuteDurationMinutes(Station startingStation, Station destinationStation);
+        public double getPriceKM(Station startingStation, Station destinationStation);
 
         public SortedSet<Station> getStartingStations();
         public SortedSet<Station> getDestinationStations(Station startingStation);
 
-        public double getPriceKM(Station startingStation, Station destinationStation);
     }
 
 }
