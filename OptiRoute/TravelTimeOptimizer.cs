f7e7e2cd6d9dab030285cb2044872c33e209cbd6
Haris Mališević <malisevic.haris@gmail.com>
2024-11-23 19:30:08 +0100

    Add: TravelTimeOptimizer.getOptimalTravelTimeMinutes

---
+++ OptiRoute/TravelTimeOptimizer.cs
@@ -0,0 +1,46 @@
+namespace OptiRoute;
+
+public static class TravelTimeOptimizer
+{
+
+    public static double getOptimalTravelTimeMinutes(List<ITransportationMethod> transportationMethod, Station startingStation, Station destinationStation)
+    {
+        if (transportationMethod == null)
+        {
+            throw new ArgumentNullException("Transportation methods must not be null.");
+        }
+
+        if (transportationMethod.Count == 0)
+        {
+            throw new ArgumentException("At least one transportation method must be provided.");
+        }
+        
+        if (startingStation == null || destinationStation == null)
+        {
+            throw new ArgumentNullException("Starting and destination stations must not be null.");
+        }
+
+        double optimalTravelTime = double.MaxValue;
+
+        foreach (var method in transportationMethod)
+        {
+            double travelTime;
+            try
+            {
+                travelTime = method.getCommuteDurationMinutes(startingStation, destinationStation);
+            }
+            catch (ArgumentException)
+            {
+
+                continue;
+            }
+
+            if (travelTime < optimalTravelTime)
+            {
+                optimalTravelTime = travelTime;
+            }
+        }
+        return optimalTravelTime;
+    }
+
+}
