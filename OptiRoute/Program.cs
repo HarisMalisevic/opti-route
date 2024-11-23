61c6fa4dae5b26efae10dd3e59aeb0721ebea0f6
Faruk Zahiragic <fzahiragic1@etf.unsa.ba>
2024-11-23 21:26:25 +0100

    Add TravelOptimizer feature.

--- OptiRoute/Program.cs
+++ OptiRoute/Program.cs
@@ -1,25 +1,23 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Principal;
 
 namespace OptiRoute
 {
     [ExcludeFromCodeCoverage]
     internal class Program
     {
-
         private static double TROLLEYBUS_PRICE_PER_ZONE_KM = 0.5;
-
         public static List<ITransportationMethod> staticTransportationMethods = new List<ITransportationMethod>{
             new Tram(StaticData.orderedTramStations),
             new Bus(StaticData.orderedBusStations, StaticData.busTravelTimesMinutes, StaticData.busTravelPricingKM),
             new Trolleybus(StaticData.orderedTrolleybusStations, StaticData.trolleybusTravelTimesMinutes, TROLLEYBUS_PRICE_PER_ZONE_KM)
         };
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
             // Prepare starting stations
-            List<Station> startingStations = prepareStartingStations(Program.staticTransportationMethods);
+            List<Station> startingStations = prepareStartingStations(staticTransportationMethods);
 
             // Print starting stations
             int index = 1;
@@ -28,7 +26,6 @@ namespace OptiRoute
                 Console.WriteLine($"{index}. {station.Name}");
                 index++;
             }
-
             // User selects starting station
             Console.WriteLine("Enter the number of the station you want to start from:");
             int selectedStationIndex;
@@ -36,12 +33,11 @@ namespace OptiRoute
             {
                 Console.WriteLine("Invalid input. Please enter a valid station number:");
             }
-
             Station selectedStartingStation = startingStations[selectedStationIndex - 1];
             Console.WriteLine($"You have selected: {selectedStartingStation.Name}");
 
             // Prepare destination stations
-            List<Station> destinationStations = prepareDestinationStations(Program.staticTransportationMethods, selectedStartingStation);
+            List<Station> destinationStations = prepareDestinationStations(staticTransportationMethods, selectedStartingStation);
 
             // Print destination stations
             index = 1;
@@ -50,38 +46,51 @@ namespace OptiRoute
                 Console.WriteLine($"{index}. {station.Name}");
                 index++;
             }
-
             // User selects destination station
             Console.WriteLine("Enter the number of the station you want to go to:");
             while (!int.TryParse(Console.ReadLine(), out selectedStationIndex) || selectedStationIndex < 1 || selectedStationIndex > destinationStations.Count)
             {
                 Console.WriteLine("Invalid input. Please enter a valid station number:");
             }
-
             Station selectedDestinationStation = destinationStations[selectedStationIndex - 1];
             Console.WriteLine($"You have selected: {selectedDestinationStation.Name}");
 
-            //TODO: Find the best route
+            //Find the best route
+
+            double optimalTravelTime = TravelTimeOptimizer.getOptimalTravelTimeMinutes(staticTransportationMethods, selectedStartingStation, selectedDestinationStation);
+
+            Console.WriteLine($"The optimal travel time is: {optimalTravelTime} minutes.");
+
+            // Prompt the user to try again or exit
+            Console.WriteLine("Would you like to find another route? (yes/no)");
+            string userResponse = Console.ReadLine().Trim().ToLower();
+
+            if (userResponse == "yes")
+            {
+                Main(args); // Restart the process
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using OptiRoute. Goodbye!");
+                Environment.Exit(0); // Exit the program
+            }
+
         }
 
         static List<Station> prepareStartingStations(List<ITransportationMethod> transportationMethods)
         {
             var startingStations = new SortedSet<Station>(new StationLexicographicComparer());
-
             for (int i = 0; i < transportationMethods.Count; i++)
             {
                 var method = transportationMethods[i];
                 var stations = method.getStartingStations();
                 startingStations.UnionWith(stations);
             }
-
             return startingStations.ToList();
         }
-
         static List<Station> prepareDestinationStations(List<ITransportationMethod> transportationMethods, Station destinationStation)
         {
             var destinationStations = new SortedSet<Station>(new StationLexicographicComparer());
-
             for (int i = 0; i < transportationMethods.Count; i++)
             {
                 var method = transportationMethods[i];
@@ -94,11 +103,9 @@ namespace OptiRoute
                 {
                     // Skip this method if it doesn't support the destination station
                     continue;
-
                 }
                 destinationStations.UnionWith(stations);
             }
-
             return destinationStations.ToList();
         }
     }
---
+++ OptiRoute/TravelOptimizer.cs
@@ -0,0 +1,44 @@
+﻿namespace OptiRoute;
+
+public static class TravelTimeOptimizer
+{
+
+    public static double getOptimalTravelTimeMinutes(List<ITransportationMethod> transportationMethod, Station startingStation, Station destinationStation)
+    {
+        if (transportationMethod == null)
+        {
+            throw new ArgumentException("Transportation methods must not be null.");
+        }
+
+        if (transportationMethod.Count == 0)
+        {
+            throw new ArgumentException("At least one transportation method must be provided.");
+        }
+
+        if (startingStation == null || destinationStation == null)
+        {
+            throw new ArgumentException("Starting and destination stations must not be null.");
+        }
+
+        double optimalTravelTime = double.MaxValue;
+
+        foreach (var method in transportationMethod)
+        {
+            double travelTime;
+            try
+            {
+                travelTime = method.getCommuteDurationMinutes(startingStation, destinationStation);
+            }
+            catch (ArgumentException) // Station not supported by this transporation method
+            {
+                continue;
+            }
+
+            if (travelTime < optimalTravelTime)
+            {
+                optimalTravelTime = travelTime;
+            }
+        }
+        return optimalTravelTime;
+    }
+}
